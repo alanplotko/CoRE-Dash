566bae00d9af4c245b8aaa701fa6a47eb49e1cb0
Alan Plotko <alan.plotko@gmail.com>
2015-03-05 02:05:27 -0500

    Implement Google+ sign-in

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+
+@app.route('/')
+@app.route('/index')
+def index():
+	#online_users = mongo.db.users.find({'online': True})
+	return render_template('index.html', template_folder=tmpl_dir)#online_users=online_users)
