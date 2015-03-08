2902c843cacd8c53986cc8c5baab086b18f94dd1
Alan Plotko <alan.plotko@gmail.com>
2015-03-08 03:31:10 -0400

    Connect to MongoDB; implement login process

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+import logging
+import json
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__)
-#client = MongoClient('localhost', 27017)
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
+client = MongoClient(os.getenv('MONGOHQ_URL'))
+db = client.core
 
 @app.route('/')
-@app.route('/index')
 def index():
-	#online_users = mongo.db.users.find({'online': True})
-	return render_template('index.html', template_folder=tmpl_dir)#online_users=online_users)
+	return render_template('index.html', template_folder=tmpl_dir, msg="CoRE Manager")
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+	if request.method == 'POST':
+		email = request.json['email']
+		name = request.json['name']
+		user = db.users.find_one({'email': email})
+		if user == None:
+			db.users.insert({
+				"name": name,
+				"email": email
+			})
+		return url_for('login')
+
+@app.route('/login')
+def login():
+	return render_template('login.html', template_folder=tmpl_dir)
