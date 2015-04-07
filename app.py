f01cf32a65443a72b903962183a791b393e77bc2
Alan Plotko <alan.plotko@gmail.com>
2015-04-06 22:45:06 -0400

    Switch to material design

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask.ext.session import Session
 from pymongo import MongoClient
 import logging
 import json
@@ -7,19 +8,31 @@ import json
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__)
 
-@app.before_first_request
-def setup_logging():
-    if not app.debug:
-        # In production mode, add log handler to sys.stderr.
-        app.logger.addHandler(logging.StreamHandler())
-        app.logger.setLevel(logging.INFO)
-
+# MongoDB Setup
 client = MongoClient(os.getenv('MONGOHQ_URL'))
 db = client.core
 
+# MongoDB Session Setup
+SESSION_TYPE = 'mongodb'
+SESSION_MONGODB = client
+SESSION_MONGODB_DB = "core"
+SESSION_MONGODB_COLLECT = "sessions"
+app.secret_key = '\xdcU\x8a\xaa\xc9\x1f\xbaVz\xbe\x06\xf9\xb9\xc5`~`\xee\xde\x92\x1b\xb4t\x80'
+app.config.from_object(__name__)
+Session(app)
+
+@app.before_first_request
+def setup_logging():
+	if not app.debug:
+		# In production mode, add log handler to sys.stderr.
+		app.logger.addHandler(logging.StreamHandler())
+		app.logger.setLevel(logging.INFO)
+
 @app.route('/')
 def index():
-	return render_template('index.html', template_folder=tmpl_dir, msg="CoRE Manager")
+	if 'logged_in' in session:
+		return redirect(url_for('dashboard'))
+	return render_template('index.html', template_folder=tmpl_dir)
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
@@ -32,6 +45,13 @@ def authenticate():
 				"name": name,
 				"email": email
 			})
+		if session.get('logged_in', None) == None:
+			_id = "session:" + str(session.sid)
+			db.sessions.update({"id": _id},	{"$set": {
+				"logged_in": "True",
+				"client_name": name,
+				"client_email": email
+			}})
 		return url_for('dashboard')
 
 @app.route('/dashboard')
@@ -40,12 +60,16 @@ def dashboard():
 
 @app.errorhandler(500)
 def internal_server(e):
-    return render_template('error.html', template_folder=tmpl_dir, error=500, error_msg="Internal Server Error", 
-    	return_home="The gears must have gotten stuck. Let us know if it happens again!"	
-    )
+	return render_template('error.html', template_folder=tmpl_dir, error=500, error_msg="Internal Server Error", 
+		return_home="The gears must have gotten stuck. Let us know if it happens again!"	
+	)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('error.html', template_folder=tmpl_dir, error=404, error_msg="Page Not Found",
-    	return_home="We can't find what you're looking for."
-    )
+	return render_template('error.html', template_folder=tmpl_dir, error=404, error_msg="Page Not Found",
+		return_home="We can't find what you're looking for."
+	)
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
--- static/assets/js/signin.js
+++ static/assets/js/signin.js
@@ -6,14 +6,10 @@ var helper = (function() {
     onSignInCallback: function(authResult) {
       gapi.client.load('plus','v1').then(function() {
         if (authResult['access_token']) {
-          $('#content').fadeOut('slow', function() {
-            $('.g-signin').hide();
-            $('#welcome').empty();
-            helper.profile();
-            $('#content').fadeIn('slow');
-          });
+          $('.g-signin').hide();
+          helper.profile();
         } else if (authResult['error']) {
-          $('#button.g-signin').show();
+          $.when($('#welcome').text("CoRE Manager")).then($('#welcome, .g-signin').fadeIn('slow'));
         }
       });
     },
@@ -21,16 +17,16 @@ var helper = (function() {
     profile: function(){
       gapi.client.plus.people.get({
         'userId': 'me'
-      }).then(function(res) {              
+      }).then(function(res) {           
         var profile = res.result;
         name = profile.displayName;
-        $('#welcome').text("Welcome back, " + name.split(" ")[0] + "!");
+        $('#content').css('margin-top', (-1 * $("#content").height()/2 - 84) + 'px');
+        $('#welcome').hide().text("Welcome back, " + name.split(" ")[0] + "!").fadeIn();
 
         for (var i=0; i < profile.emails.length; i++) {
           if (profile.emails[i].type === 'account') primaryEmail = profile.emails[i].value;
         }
 
-        $("#content").css('margin-top', (-1 * $("#content").height()/2 - 84) + 'px');
         $('#spinner').delay(1000).fadeIn('slow');
       }, function(err) {
         var error = err.result;
@@ -48,7 +44,7 @@ var helper = (function() {
         url: "/authenticate",
         type: "POST",
         contentType: "application/json",
-        data: JSON.stringify  ({
+        data: JSON.stringify({
           "email": email,
           "name": name
         }),
