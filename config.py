ceb15f81c4cab9be3e0961edfcec7a74bac73113
Alan Plotko <alan.plotko@gmail.com>
2015-08-17 15:00:27 -0400

    Move keys to environment

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import authomatic
 from authomatic.providers import oauth2
+
 CONFIG = {
-    
     'google': {
            
         'class_': oauth2.Google,
         'id': authomatic.provider_id(),
 
         # Google is an AuthorizationProvider too.
-        'consumer_key': '615316435724-k7gu8chlmqc3gkuru56bamcr4f9pv7sm.apps.googleusercontent.com',
-        'consumer_secret': 'gSkFr02eGVLVXrlrRqbfBBN_',
+        'consumer_key': os.getenv('GOOGLE_KEY'),
+        'consumer_secret': os.getenv('GOOGLE_SECRET'),
         
         # But it is also an OAuth 2.0 provider and it needs scope.
         'scope': ['https://www.googleapis.com/auth/plus.login profile email'],
