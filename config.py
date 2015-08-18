4994910c1f27eb70e3dfe0381bb59837cfb78012
Alan Plotko <alan.plotko@gmail.com>
2015-08-18 11:24:53 -0400

    Update config.py
    Rename variables

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ CONFIG = {
         'id': authomatic.provider_id(),
 
         # Google is an AuthorizationProvider too.
-        'consumer_key': os.getenv('GOOGLE_KEY'),
-        'consumer_secret': os.getenv('GOOGLE_SECRET'),
+        'consumer_key': os.getenv('COREDASH_GOOGLE_KEY'),
+        'consumer_secret': os.getenv('COREDASH_GOOGLE_SECRET'),
         
         # But it is also an OAuth 2.0 provider and it needs scope.
         'scope': ['https://www.googleapis.com/auth/plus.login profile email'],
